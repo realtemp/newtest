d5af47fced4f7e82bbbc89f6c6e07ddb4d0758b1
liuxudong <liuxudong@ceopen.cn>
2010-11-05 10:42:03 +0800

    Signed-off-by: liuxudong <liuxudong@ceopen.cn>

---
+++ src/com/ceopen/test/Testgit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ceopen.test;
+
+/**
+ * @author realaicy
+ *
+ */
+public class Testgit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
